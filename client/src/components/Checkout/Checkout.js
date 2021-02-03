ea6293e64df12bf9a7425dcca70eb02dc7c3633c
Michael Chapa <michaelchapa1@gmail.com>
2021-02-03 13:21:15 -0600

    Completed migration of React front-end onto Node.js backend. API can interact with Homepage Component, will need to interact with Checkout component.

---
+++ client/src/components/Checkout/Checkout.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import { withRouter } from 'react-router-dom'
+import ShippingForm from './ShippingForm/ShippingForm'
+import { CartContext } from '../CartContext'
+
+function Checkout({ history }) {
+    // eslint-disable-next-line
+    const [cart, setCart, totalPrice, itemCount, cartSummary, 
+        sidebar, setSidebar] = useContext(CartContext);
+
+    function goto(step) { 
+        if(step === 0) {
+            history.push('/cart');
+        }
+    }
+
+    return (
+        <div>
+            <h1>Checkout</h1>
+            {cartSummary}
+            <span className = "total-label">Total:</span>
+            <span className = "total-number">${totalPrice}</span>
+            <ShippingForm />
+            <p>3. Calculate Shipping</p>
+            <p>4. Collect Payment Information</p>
+            <p>5. SUBMISSION :O</p>
+            <button onClick = {() => goto(0)}>Edit Cart</button>
+            <input type = "submit" value = "Complete Order" />
+        </div>
+    )
+}
+
+export default withRouter(Checkout);
